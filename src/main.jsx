59a28f09fdf280ee00272294e7b0481a042e981d
Arnaud R. <arno.royer2004@gmail.com>
2025-10-06 11:46:43 +0200

    Feat: add TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  { path: '/', element: <App />, children: [
+
+  ] }
+]);
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const rootApp = createRoot(rootElement);
+  rootApp.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+}
